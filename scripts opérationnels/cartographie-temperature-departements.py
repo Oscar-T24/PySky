14b856b8dd58a62577384101c21e3846a9c0b9a5
Oscar-T24 <o.tesniere24@ejm.org>
2023-04-20 08:14:43 +0200

    update

--- scripts opérationnels/cartographie-temperature-departements.py
+++ scripts opérationnels/cartographie-temperature-departements.py
@@ -198,4 +198,4 @@ def map():
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=4650)
 '''
-"""#a#aaaa
+"""#a#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
