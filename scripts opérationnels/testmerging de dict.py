afffffd8da1e6140d2665228dd8b588a3ce79c72
Oscar-T24 <116503103+Oscar-T24@users.noreply.github.com>
2023-04-20 17:16:21 +0200

    correction

--- scripts opérationnels/testmerging de dict.py
+++ scripts opérationnels/testmerging de dict.py
@@ -1,24 +1,37 @@
 # example input data
-list1 = [{'Code': '00', 'Name': 'John'}, {'Code': '01', 'Name': 'Mary'}, {'Code': '03', 'Name': 'Peter'}]
-list2 = [{'Code': '00', 'Age': 25}, {'Code': '02', 'Age': 30}, {'Code': '03', 'Age': 35}]
-
-# create a set of all possible 'Code' values
-possible_codes = set(['{:02d}'.format(code) for code in range(92)])
+list1 = [{'Code': '73', 'Temperature': '3.4', 'etat': 'Cloudy'}, {'Code': '84', 'Temperature': '7.2', 'etat': 'Sunny'}, {'Code': '91', 'Temperature': '12.3', 'etat': 'Rainy'}]
+list2 = [{'Code': '973', 'departement': 'Guyane', 'coordonnee': '[4.0039882, -52.999998]'}, {'Code': '84', 'departement': 'Gironde', 'coordonnee': '[44.837789, -0.57918]'}, {'Code': '73', 'departement': 'Somme', 'coordonnee': '[49.96237, 2.35893]'}, {'Code': '09', 'departement': 'Ariège', 'coordonnee': '[42.9167, 1.0833]'}]
 
 # create a dictionary from each input list where the keys are the 'Code' values
 dict1 = {d['Code']: d for d in list1}
 dict2 = {d['Code']: d for d in list2}
 
-# merge the two dictionaries into a single dictionary
-merged_dict = {}
-for code in possible_codes:
-    if code in dict1:
-        merged_dict[code] = dict1[code]
-    elif code in dict2:
-        merged_dict[code] = dict2[code]
+# loop over the missing dictionaries in list1 and update with coordonnee if found in list2
+for d in list1:
+    if d['Code'] in dict2:
+        d2 = dict2[d['Code']]
+        d.update({'coordonnee': d2['coordonnee']})
+    else:
+        d.update({'coordonnee': ''})
 
-# convert the merged dictionary back into a list of dictionaries
-merged_list = list(merged_dict.values())
-merged_list = sorted(merged_list, key=lambda merged_list: merged_list['Code']) 
+# merge the updated dictionaries from list1 and the dictionaries from list2 into a single dictionary
+merged_dict = {**dict1, **dict2}
+
+# create a new list of dictionaries with the desired keys/values
+# create a new list of dictionaries with the desired keys/values
+# create a new list of dictionaries with the desired keys/values
+merged_list = []
+for code, d in merged_dict.items():
+    if code in dict1:
+        # retrieve 'Temperature' and 'etat' from the dictionary in list1
+        temperature = dict1[code].get('Temperature', None)
+        etat = dict1[code].get('etat', None)
+        merged_list.append({'Code': code, 'Temperature': temperature, 'etat': etat, 'coordonnee': d.get('coordonnee', '')})
+    else:
+        # only keep 'Code' and 'coordonnee' for missing dictionaries
+        merged_list.append({'Code': code, 'coordonnee': d.get('coordonnee', '')})
+
 # print the result
 print(merged_list)
+
+
