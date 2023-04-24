2bce0510d53d625a9471b524420a6ff65ba215b0
Pi-ckle <121044774+Pi-ckle@users.noreply.github.com>
2023-04-24 21:37:55 +0200

    mise a jour tables

--- scripts opérationnels/cartographie-temperature-departements.py
+++ scripts opérationnels/cartographie-temperature-departements.py
@@ -199,3 +199,4 @@ if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=4650)
 '''
 """#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+a
