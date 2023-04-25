844cc6e32d9ef9d1ab46bea8ee04c739162bd79a
Pi-ckle <121044774+Pi-ckle@users.noreply.github.com>
2023-04-25 17:08:08 +0200

    update normale

--- scripts opérationnels/normales.py
+++ scripts opérationnels/normales.py
@@ -13,15 +13,16 @@ with open('normale.csv', 'w', newline='') as n:
     for e in coordonnees:
         c = e['coordonnee'].split(" ")
         d, x, y = e['Code'], float(c[0][1:-2]), float(c[-1][:-2])
-        print(f"Calcul de l'humidité moyenne par mois du département {d}")
+        print(f"Calcul de la precipitation moyenne par mois du département {d}")
         normales_dep = [d]
         for mois in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]:
             normales_dep_mois = []
             for year in range(today.year - 31, today.year - 1):
-                data = pd.read_json(f'https://archive-api.open-meteo.com/v1/archive?latitude={x}&longitude={y}&start_date={year}-{mois}-15&end_date={year}-{mois}-15&hourly=temperature_2m,relativehumidity_2m,precipitation,soil_moisture_0_to_7cm')
+                data = pd.read_json(f'https://archive-api.open-meteo.com/v1/archive?latitude={x}&longitude={y}&start_date={year}-{mois}-15&end_date={year}-{mois}-15&daily=precipitation_sum&timezone=Europe%2FLondon')
                 #print(data.to_string())
-                liste = data.values.tolist()[2][-1] # 2 car c'est le troisième paramètre que l'on appelle de l'API
-                #print(liste)
-                normales_dep_mois.append(round(sum(liste)/len(liste), 4))
+                precipitation = data.values.tolist()[1][-1][0] # 2 car c'est le troisième paramètre que l'on appelle de l'API
+                #print(precipitation)
+                normales_dep_mois.append(precipitation)
             normales_dep.append(round(sum(normales_dep_mois)/len(normales_dep_mois), 4))
         normale.writerow(normales_dep)
+        break
