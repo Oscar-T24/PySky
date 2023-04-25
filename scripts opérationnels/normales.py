3cac1829e22f560b24cf3bddaa02086101d0a959
Pi-ckle <121044774+Pi-ckle@users.noreply.github.com>
2023-04-25 17:11:47 +0200

    normale update

--- scripts opérationnels/normales.py
+++ scripts opérationnels/normales.py
@@ -17,7 +17,7 @@ with open('normale.csv', 'w', newline='') as n:
         normales_dep = [d]
         for mois in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]:
             normales_dep_mois = []
-            for year in range(today.year - 31, today.year - 1):
+            for year in range(today.year - 11, today.year - 1):
                 data = pd.read_json(f'https://archive-api.open-meteo.com/v1/archive?latitude={x}&longitude={y}&start_date={year}-{mois}-15&end_date={year}-{mois}-15&daily=precipitation_sum&timezone=Europe%2FLondon')
                 #print(data.to_string())
                 precipitation = data.values.tolist()[1][-1][0] # 2 car c'est le troisième paramètre que l'on appelle de l'API
@@ -25,4 +25,3 @@ with open('normale.csv', 'w', newline='') as n:
                 normales_dep_mois.append(precipitation)
             normales_dep.append(round(sum(normales_dep_mois)/len(normales_dep_mois), 4))
         normale.writerow(normales_dep)
-        break
