2aa95c98f3cdead4e2c560254cd3d2c880838e96
Oscar-T24 <116503103+Oscar-T24@users.noreply.github.com>
2023-04-26 23:15:17 +0200

    update

--- scripts opérationnels/cartographie.py
+++ scripts opérationnels/cartographie.py
@@ -65,7 +65,7 @@ with open('donnees_meteo.csv', 'r') as f:
 descripteurs = ['Temperature (°C)','Humidite_relative (%)','Temperature_ressentie (°C)','Probabilite_pluie (%)','Precipitation (mm)','Pression (0m)(hPa)','Couverture_nuageuse (%)','Visibility (m)','Vitesse_vent (km/h)','Index_UV','River_discharge (m3/s)','Probabilité sècheresse','Probabilité innondation']
 # on modifie expressement les descripteurs pour n'inclure que ceux qu'on veux mettre en cloropleth
 for donnee in descripteurs:
-    folium.Choropleth(  # premier cloropleth pour la temperature
+    a = folium.Choropleth(  # premier cloropleth pour la temperature
         geo_data=geojson_data,
         name=f"{donnee} à {dt_string}",
         data=merged_data,
@@ -80,7 +80,12 @@ for donnee in descripteurs:
         highlight=True,
         show=False,
         overlay=True,
-    ).add_to(m)
+    )
+    # SUPPRIMER LES LEGENDES
+    for key in a._children:
+        if key.startswith('color_map'):
+            del(a._children[key])      
+    a.add_to(m)
 
 # folium.Marker(location=[46, 6], icon=folium.Icon(icon='sun')).add_to(m)
 # https://www.python-graph-gallery.com/312-add-markers-on-folium-map?utm_content=cmp-true
@@ -164,10 +169,16 @@ fg.add_child(folium.LayerControl())
 '''
 folium.TileLayer('cartodbpositron', name='carte simplifiee').add_to(m)
 
+layer_control = folium.LayerControl(position='topright', collapsed=True)
+m.add_child(layer_control)
+ 
+
+
+'''
 folium.LayerControl(
     collapsed=True,
 ).add_to(m)  # ajouter le panneau de controle
-
+'''
 # Display the map
 
 
