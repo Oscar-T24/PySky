1c0b38875071ec213ad75878f911d97daaa13c4a
Oscar-T24 <116503103+Oscar-T24@users.noreply.github.com>
2023-04-28 21:37:13 +0200

    update

--- scripts opérationnels/main.py
+++ scripts opérationnels/main.py
@@ -31,5 +31,3 @@ with open('temps_ecoulement.txt', 'a') as f:
     f.write('\n')
 # temps pour faire les étapes 2 à 4 : 4 minutes +/- 10s
 # temps pour la partie 1 : 160,7 minutes soit 2,67 heures
-
-os.remove("donnees_meteo.csv")
