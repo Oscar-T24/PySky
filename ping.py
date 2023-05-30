4d19eec2615de9b6f44ac03a9b16f3c0c9f91846
Oscar-T24 <116503103+Oscar-T24@users.noreply.github.com>
2023-05-30 17:04:47 +0200

    ajout d'un script pour ping

---
+++ ping.py
@@ -0,0 +1,7 @@
+import subprocess
+import platform
+
+host = "93.14.22.225"
+connectivite = not(subprocess.call(['ping', '-n' if platform.system().lower()=='windows' else '-c', '1', host,'-p','1025']))
+with open('etat_serveur.txt','w') as f:
+    f.write(connectivite)
